b75f1202be003ccb61109fa05f1adb6096f335c2
LiborPezinek <lpezinek@gmail.com>
2026-05-09 11:47:21 +0200

    doplnene ify pro prehlednost

--- main.py
+++ main.py
@@ -6,11 +6,14 @@
 ## 		  Původní dataset obsahuje data od roku 1981 do roku 2025
 
 import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
+import matplotlib.pyplot as plt
 
 import utils
 
 
 def main() -> None:
+	# Load data
 	time, values = utils.LoadCsvData("data/QD_109000_Data.csv")
 
 	# Log-transform the data to stabilize variance
@@ -20,37 +23,60 @@ def main() -> None:
 	valuesDetrended = values.diff().dropna()
 
 	# Calculate the trend using a rolling mean with a window of 365 days (1 year)
-	trend = valuesLog.rolling(window=365).mean().dropna()	# just for visualization purposes, not used in further analysis
+	trend = valuesLog.rolling(window=365).mean().dropna()
 
+	# 2:  Data vykreslete, posuďte autokovarianční funkci.
+	# 3:  Ověřte, zda je třeba provést transformaci stabilizující rozptyl a případně ji proveďte.  
+	# 4:  Odstraňte trend vhodnou metodou.
+	# 5:  Identifikujte periodu sezónní složky (z podstaty dat, ověřte periodogramem)
+	# 6:  Odhadněte trendovou a sezónní složku
+	# 7:  Testem náhodnosti ověřte, zda získaná rezidua jsou IID a zda jsou normální
+	# 8:  Vykreslete autokorelační funkci a parciální autokorelační funkci reziduí a pokuste se určit vhodný ARMA model
+	# 9:  Určete predikci o h kroků dopředu buď užitím předchozích kroků nebo pomocí SARIMA.
+	task = 6
 
-	# # Visualize original data
-	# utils.VisualiseData(time, values, ylabel = "Průtok (m³/s)", title = "Průtoková data ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
+	# 2:  Data vykreslete, posuďte autokovarianční funkci.
+	if task == 2:
+		# Visualize original data
+		utils.VisualiseData(time, values, ylabel = "Průtok (m³/s)", title = "Průtoková data ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
 	
-	# # Calculate and plot autocovariance function
-	# utils.CalcAndPlotACVF(time, values, title = "Autocovarianční funkce pro průtoková data")
+		# Calculate and plot autocovariance function
+		utils.CalcAndPlotACVF(time, values, title = "Autocovarianční funkce pro průtoková data")
 
-	# # Calculate and plot rolling variance - to know whether to transform or not
-	# utils.CalcAndPlotRollingVariance(time, values, title = "Klouzavý rozptyl průtokových dat")
+	# 3:  Ověřte, zda je třeba provést transformaci stabilizující rozptyl a případně ji proveďte.  
+	if task == 3:
+		# Calculate and plot rolling variance - to know whether to transform or not
+		utils.CalcAndPlotRollingVariance(time, values, title = "Klouzavý rozptyl průtokových dat")
 
-	# # Plot log transformed data
-	# utils.VisualiseData(time, valuesLog, ylabel = "Logaritmus průtoku (log(m³/s))", title = "Transformovaná data (logaritmus) ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
+		# Plot log transformed data
+		utils.VisualiseData(time, valuesLog, ylabel = "Logaritmus průtoku (log(m³/s))", title = "Transformovaná data (logaritmus) ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
 
-	# # Re-calculate and plot rolling variance for transformed data
-	# utils.CalcAndPlotRollingVariance(time, valuesLog, title = "Klouzavý rozptyl logaritmovaných průtokových dat")
+		# Re-calculate and plot rolling variance for transformed data
+		utils.CalcAndPlotRollingVariance(time, valuesLog, title = "Klouzavý rozptyl logaritmovaných průtokových dat")
 
-	# # Show trend
-	# utils.VisualiseData(time[365:], trend, ylabel = "Trend (log(m³/s))", title = "Trend logaritmovaných průtokových dat ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
+	# 4:  Odstraňte trend vhodnou metodou.
+	if task == 4:
+		# Show trend
+		utils.VisualiseData(time[365:], trend, ylabel = "Trend (log(m³/s))", title = "Trend logaritmovaných průtokových dat ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
 
-	# # Plot detrended data
-	# utils.VisualiseData(time[1:], valuesDetrended, ylabel = "Diferencovaná průtoková data (log(m³/s))", title = "Sezónně diferencovaná data ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
+		# Plot detrended data
+		utils.VisualiseData(time[1:], valuesDetrended, ylabel = "Diferencovaná průtoková data (log(m³/s))", title = "Diferencovaná data ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
 	
-	# # Calculate and plot autocovariance function for seasonally differenced data
-	# utils.CalcAndPlotACVF(time[1:], valuesDetrended, title = "Autocovarianční funkce pro diferencovaná data")
+		# Calculate and plot autocovariance function for seasonally differenced data
+		utils.CalcAndPlotACVF(time[1:], valuesDetrended, title = "Autocovarianční funkce pro diferencovaná data")
 
-	# # # Calculate and plot autocovariance function with period annotation
-	periods, power = utils.CalcAndPlotPeriodogram(values, title = "Periodogram průtokových dat ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
-	peakPeriod = periods[np.argmax(power)]
-	print(peakPeriod)	# 365.25 - confirming the yearly seasonality
+	# 5:  Identifikujte periodu sezónní složky (z podstaty dat, ověřte periodogramem)
+	if task == 5:
+		# Calculate and plot periodogram to identify seasonality
+		periods, power = utils.CalcAndPlotPeriodogram(values, title = "Periodogram průtokových dat ze stanice Vyšší Brod, řeka Vltava (2010-2025)")
+		peakPeriod = periods[np.argmax(power)]
+		print(peakPeriod)	# 365.25 - confirming the yearly seasonality
+
+	# 6:  Odhadněte trendovou a sezónní složku
+	if task == 6:
+		decompTSR = seasonal_decompose(valuesLog, model='additive', period=365)
+		decompTSR.plot()
+		plt.show()
 
 
 
